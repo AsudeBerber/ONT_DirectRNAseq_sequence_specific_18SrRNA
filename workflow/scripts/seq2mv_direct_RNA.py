0348668285533973db7aca1258fcd35fa92dee73
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-03 11:47:26 +0200

    imported function for signal plotting

---
+++ workflow/scripts/seq2mv_direct_RNA.py
@@ -0,0 +1,256 @@
+__author__ = "Jens Martin"
+__email__ = "jens.martin@outlook.com"
+
+import argparse
+import os
+import pod5 as p5
+import matplotlib.pyplot as plt
+import numpy as np
+import pysam as ps
+
+
+#creates textfile with all read_ids within bam file
+def read_id_list_bam(bam_dir = None, sample=None):
+    try:
+        f = open("data/read_id_list_bam.txt", "x")
+    except:
+        # if file exists, it is wiped
+        f = open("data/read_id_list_bam.txt", "w")
+        f.write("")
+        f.close()
+
+    f = open("data/read_id_list_bam.txt", "a")
+
+    with ps.AlignmentFile(f"../../data/mapped/{bam_dir}/{sample}.bam") as samfile:
+        samfile.fetch()
+        for reads in samfile.fetch():
+            f.write(str(reads.query_name) +"\n")
+            print(reads.query_name)
+    f.close()
+
+
+#gets movetable (mv), ts and corresponding base sequence (seq) for given read id
+def bam_aligned(bam_dir, sample, read_ids, region=None):
+
+    samfile = ps.AlignmentFile(f"../../data/mapped/{bam_dir}/{sample}.bam")
+
+    max_reads = samfile.mapped
+    i = 0
+        
+    for read in samfile.fetch(region = region):
+        if read.query_name == read_ids:
+            read_ID = read.query_name
+            seq = read.query_alignment_sequence
+            seq = seq[::-1] #sequence order is 5' -> 3', mv and signal are 3' -> 5'
+            read.set_tag("ts", None) #first ts tag is transcript strand, has to be removed
+            mv = read.get_tag("mv")
+            ts = read.get_tag("ts")
+            # print(read.get_tags())
+            # print(f"ts:{ts}; {read_ID}") 
+            return(seq, mv, ts)
+        else: 
+            #removing the else part makes the code only 1s faster
+            i = i+1
+            k = i/500000
+            if k.is_integer():
+                # print("currently at " +read.query_name + "\n")
+                print(f"reads compared: {i} of max. {max_reads}")
+            continue
+
+
+
+# assigns individual bases in sequence to corresponding part of signal through movetable information
+def seq_to_mv(reads_ids, bam_dir, sample, region, seq=None, mv=None, ts=0, fetch = True):
+    if fetch == True:
+        seq, mv, ts = bam_aligned(bam_dir, sample, reads_ids, region)
+
+    s = mv[0] #stride length
+    p = 1 #itinerates through movetable array
+    x = 0 #number of additional strides (stride amount - 1)
+    start = ts + 1
+
+    seq2mv = np.array([[1, ts, "-"]])
+    for base in seq:
+        # print (base)
+        while p < len(mv)-1 and mv[p + 1] == 0: #last movetable index(p): mv[p+1] doesn't exist, would cause error
+            x = x + 1 #counts 0's
+            p = p + 1 #0 found -> movetable index moves by one
+            # print (f"{p},{x}\n")
+        p = p + 1 
+        # print(f"p={p}")
+        # print(f"x={x}")
+        end = start + (s-1) + (x*s) #stride 1: start + 4 (as start number is already first position in stride), all further strides: additional +5
+        x = 0 #resets number of additional strides
+        seq2mv = np.append(values=[[start, end, base]], arr=seq2mv, axis=0) # appends 
+        start = end+1 #next base starts 1 after end of previous base
+
+    print ("sequence-to-signal alignment finished,", len(seq), "bases, signal length =", end)    
+    return seq2mv
+
+
+def reverse_seq_mv(seq2mv):
+    k = int(seq2mv[-1][1]) #signal range
+    rev_seq2mv = np.flip(seq2mv,0)
+    for base_data in rev_seq2mv:
+        start_old = int(base_data[0])
+        end_old = int(base_data[1])
+        base_data[0] = k - end_old #start and end switch when string is read in other direction, former last base is now first and vice versa
+        base_data[1] = k - start_old
+    return rev_seq2mv
+
+
+#writes generated array to txt file
+def seq2mv_to_txt(seq2mv):
+    try:
+        a = open("../../data/seq2mv.txt", "x")
+    except: 
+        # if file exists, it is wiped
+        a = open("../../data/seq2mv.txt", "w")
+        a.write("")
+        a.close()
+
+    a = open("../../data/seq2mv.txt", "a")
+    for line in seq2mv:
+        a.write(" ".join(line) + "\n")
+    a.close()
+
+
+#assigns every base a color for annotation
+def base_color(base):
+    if base == "A": col = "#E9452C"
+    elif base == "C": col = "#4596F7" 
+    elif base == "T": col = "#55B73B"
+    elif base == "U": col = "#F3B642" 
+    elif base == "G": col = "#F3B642" 
+    elif base == "X": col = "#00ffc3" #for acetylation?
+    else: col = "black"
+    return col
+
+
+#plots array of [start, end, base] to position (start - end) in signal
+def plot_signal_plus_seq(seq2mv, read_ids, start, end, bam_dir, full_read=False, range_var = "bases", pod5_dir = "pod5"):
+     
+    if pod5_dir == None:
+        pod5_dir = "../../data/pod5"
+    else: 
+        pod5_dir = f"../../data/{pod5_dir}"
+
+    for filename in os.listdir(pod5_dir): #loops through all pod5 files in folder 
+        pod5_file = os.path.join(pod5_dir, filename)
+        with p5.Reader(pod5_file) as reader:
+            # Read the selected read from the pod5 file
+            # next() is required here as Reader.reads() returns a Generator
+            try:
+                read = next(reader.reads([read_ids]))
+            except:
+                continue
+            #
+            signal = read.signal
+            signal = signal[::-1] #reverses signal as RNA is sequenced 3' -> 5' but sequence is read 5' -> 3'
+#
+            # when range is set by bases, starting and ending time are pulled from seq2mv array --> base range is converted to time range
+            time = np.arange(len(signal)) #arbitrary time units
+            if range_var == "bases":
+                print("plot for base range", start, "-", end)
+                start = int(seq2mv[start][0])
+                end = int(seq2mv[end][1])
+                print("corresponding time range:", start,"-", end)
+            elif range_var == "time":
+                pass
+            else:
+                raise Exception ("range_var has to be bases or time")
+        #
+        #   
+            #
+            if full_read == True:
+                start = 1
+                end = len(signal)
+            #
+            signal_slice = signal[start:end]
+            time = np.arange(len(signal)) #arbitrary time units
+            time_slice = time[start:end]
+            #
+            # Plot using matplotlib
+            fig, ax = plt.subplots(figsize=(18, 4))
+            #
+            ax.plot (time_slice, signal_slice,linewidth = 1, color = "#4F849E", zorder = 1)
+            ax.scatter(time_slice, signal_slice,linewidth = 1, marker= "o", facecolor = "#009BE6", zorder = 2, alpha = 0.8, edgecolor = "none")
+            ax.margins(0.05, 0.1)
+            ax.set(xlabel = "time", ylabel = "Signal (pA)")
+            plt.title(str("Read ID: "+ read_ids))
+#
+            #annotation of bases to signal plot
+            for base_data in seq2mv:
+                x_coord = (int(base_data[0])+int(base_data[1]))/2 
+                if x_coord < start:
+                    pass
+                elif x_coord > start and x_coord < end: 
+                    ax.annotate(base_data[2], xy = (x_coord+0.25, 0.02), fontsize = 7, xycoords=("data", "axes fraction"), ha = "center", color = base_color(base_data[2]))
+                    ax.axvline(int(base_data[0]), linestyle = ":", linewidth = 0.5, color = "lightgrey")
+                else:
+                    ax.axvline(int(base_data[0])-1, linestyle = ":", linewidth = 0.5, color = "lightgrey")
+                    break
+
+        # check if plot dir exists, creates it otherwise
+        if not os.path.isdir(f"../../data/mapped/{bam_dir}/plots/{read_ids}"):
+            os.makedirs(f"../../data/mapped/{bam_dir}/plots/{read_ids}")
+
+        plt.savefig(f"../../data/mapped/{bam_dir}/plots/{read_ids}/{read_ids}_{start}-{end}.png", dpi = 300)           
+        plt.show()
+
+
+# saves list of all read_ids in bamfile to ../data
+# read_id_list_bam()
+    
+
+
+##########################################################################################################################################################
+# exemplary command line argument:
+#
+# python seq2mv_direct_RNA.py noisy_corr RNAseq_test_noisy_correction_sorted becda29e-a3f7-4647-a0b4-1dfd9134c3cc 300 350 --region SIRV6001:16-6000
+#
+# if known, specify region/reference span(=chromosome) (e.g. "ERCC-00076[:start-end]" ; []<- this part is optional"); time for execution will be much slower otherwise (minutes range)
+# for region argument thousand seperators have to be removed, e.g. SIRV6001:16-6000 works, SIRV6001:16-6.000 (as given by IGV) does not
+######################################################################################################################################################
+        
+
+parser = argparse.ArgumentParser(
+                    description="plots electric current/timepoint with corresponding basecalled base for given Read_ID")
+parser.add_argument("bam_dir", help= "directory of bamfile, i.e.: path= ../../data/mapped/[bam_dir]/[sample].bam")
+parser.add_argument("sample", help= "Name of sample bam file w/o .bam ending", action="store")
+parser.add_argument("readID", help= "Sample ID in bam and pod5 file", action="store")
+parser.add_argument("start", help= "index of first base to show in plot, start with 0", type=int, action="store")
+parser.add_argument("end", help= "index of last base to show in plot", type=int, action="store")
+parser.add_argument("--no_fetch", help= "Disables fetching mv, ts, seq from readID - requieres --mv, --ts, --seq", action="store_true")
+parser.add_argument("--seq", help= "complete sequence of read", action="store_true")
+parser.add_argument("--mv", help= "movetable", action="store_true")
+parser.add_argument("--ts", help= "", action="store_true")
+parser.add_argument("--get_readids", action= "store_true")
+parser.add_argument("--region", type= str, action= "store")
+parser.add_argument("--pod5_dir", type= str, action= "store")
+args = parser.parse_args()
+
+if args.get_readids == True:
+    read_id_list_bam(args.bam_dir, args.sample)
+
+if args.no_fetch == True:
+    fetch = False
+else:
+    fetch = True 
+
+seq2mv = seq_to_mv(reads_ids = args.readID, bam_dir = args.bam_dir, sample = args.sample, region =args.region,
+                    seq = args.seq, mv=args.mv, ts=args.ts, fetch=fetch)  
+
+
+# as RNA is sequenced 3' -> 5', but convention + basecalled sequence is 5' -> 3' the seq2mv has to be "turned around" 
+# for 5' -> 3' sequencing direction, only this part has to be removed
+rev_seq2mv=reverse_seq_mv(seq2mv)
+
+
+#plots array to signal
+plot_signal_plus_seq(rev_seq2mv, read_ids = args.readID, start = args.start, end = args.end, bam_dir = args.bam_dir, full_read=False, pod5_dir = args.pod5_dir)
+
+
+#saves seq2mv array (base aligned to signal position) to txt file in ../data
+# seq2mv_to_txt(seq2mv) 
+# seq2mv_to_txt(rev_seq2mv)
