ff974111fd0d54c1ba2d8958c1cb42ce5d24b260
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-04 15:54:11 +0200

    fix

--- workflow/scripts/seq2mv_direct_RNA.py
+++ workflow/scripts/seq2mv_direct_RNA.py
@@ -209,7 +209,7 @@ def plot_signal_plus_seq(seq2mv, read_ids, pos, pos_read, range, sequencer, full
                     ax.annotate(base_data[2], xy = (x_coord+0.25, 0.02), fontsize = 7, xycoords=("data", "axes fraction"), ha = "center", color = base_color(base_data[2]))
                     ax.annotate(i, xy= (x_coord+0.25, -0.04), fontsize = 7, xycoords=("data", "axes fraction"), ha = "center")
                     ax.axvline(int(base_data[0]), linestyle = ":", linewidth = 0.5, color = "lightgrey")
-                    xticks = xticks.append(x_coord)
+                    xticks.append(x_coord)
                     i = i + 1
                 else:
                     ax.axvline(int(base_data[0])-1, linestyle = ":", linewidth = 0.5, color = "lightgrey")
