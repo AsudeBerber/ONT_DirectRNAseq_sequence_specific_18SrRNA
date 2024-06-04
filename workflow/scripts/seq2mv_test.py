9cf07e145a1ad22a8cbf999fa34c1a4d4cbf3c31
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-04 13:52:58 +0200

    test

--- workflow/scripts/seq2mv_test.py
+++ workflow/scripts/seq2mv_test.py
@@ -29,9 +29,9 @@ for read in samfile.fetch():
             #print(read.get_tags())
             #print(f"ts:{ts}; {read_ID}") 
             aligned_pairs = read.get_aligned_pairs()
-            for i, aln, ref in enumerate(aligned_pairs):
-                 if ref == 1337:
-                      print(i, aln, ref)
+            for i, pair in enumerate(aligned_pairs):
+                 if pair[1] == 1337:
+                      print(i, pair)
             print(aligned_pairs.index(int, 1337))
         else: 
             #removing the else part makes the code only 1s faster
