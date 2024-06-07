61d3e350d2caf7981cd3a5d4e401fdd0adb9981f
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-07 11:25:53 +0200

    debug

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -130,6 +130,7 @@ def main(argv=sys.argv[1:]):
 
             # extract features from bam file
             print (loci) 
+            print(read.qual[locus-extra_window: locus+motif_length+extra_window])
             per_site_qual = np.array([list(read.qual[locus-extra_window: locus+motif_length+extra_window]) for locus in loci])
             per_site_query_seq = np.array([list(read.query_sequence[locus-extra_window: locus+motif_length+extra_window]) for locus in loci])
             seq_dict = dict((x, z) for x, y, z in aligned_pairs)
