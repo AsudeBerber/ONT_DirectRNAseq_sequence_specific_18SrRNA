96daba4f1c4d3d9de971e0d5fe211e37c00700b5
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-10 11:18:37 +0200

    test

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -150,7 +150,7 @@ def main(argv=sys.argv[1:]):
             #loops through all pod5 files in folder 
             with p5.DatasetReader(pod5_file, threads= 8) as dataset:
                 for read_record in dataset.reads(): 
-                    
+                    breakpoint()
                 # with p5.Reader(pod5_file) as pod5:
                     # Read the selected read from the pod5 file
                     # next() is required here as Reader.reads() returns a Generator
