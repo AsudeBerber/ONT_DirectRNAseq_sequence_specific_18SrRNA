1adc6fd88d3cd76fc998bc1d64f8d2a57f5e7ba4
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-10 12:37:41 +0200

    test

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -151,11 +151,10 @@ def main(argv=sys.argv[1:]):
             with p5.DatasetReader(pod5_file, threads= 8, recursive= True) as dataset:
                 i = 0
                 k = 0
-                for read_record in dataset.reads(): 
-                    
                     # with p5.Reader(pod5_file) as pod5:
                     # Read the selected read from the pod5 file
                     # next() is required here as Reader.reads() returns a Generator
+                    pod5_record = next(dataset.reads(selection=[read.query_name]))
                     if (read_record.read_id == read.query_name):
                         events = get_events(read_record.signal, read.get_tag("mv"), read.get_tag("ts"))
                         per_site_features = [events[locus-extra_window: locus+motif_length+extra_window] for locus in loci]
