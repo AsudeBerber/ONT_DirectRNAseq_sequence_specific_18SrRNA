0066055aea31837936a154ddffe3ad1f3de73ed2
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-10 11:51:37 +0200

    test

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -156,7 +156,7 @@ def main(argv=sys.argv[1:]):
                     # next() is required here as Reader.reads() returns a Generator
                     try:
                         pod5_record = next(read_record(selection=[read.query_name])) 
-                        events = get_events(read_record.signal, read.get_tag("mv"), read.get_tag("ts"))
+                        events = get_events(pod5_record.signal, read.get_tag("mv"), read.get_tag("ts"))
                         per_site_features = [events[locus-extra_window: locus+motif_length+extra_window] for locus in loci]
                         per_site_id = [read.query_name + ':' + str(locus) for locus in loci]
 
