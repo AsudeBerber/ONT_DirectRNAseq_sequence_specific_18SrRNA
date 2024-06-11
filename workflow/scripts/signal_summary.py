c2fcf5cbee420cf66265887fca9eefccf3307ee0
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-11 09:31:38 +0200

    test

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -173,12 +173,14 @@ def main(argv=sys.argv[1:]):
             time_pod = time.process_time() - time_st
             print (f"time pod5 loop: {time_pod}")
 
-            breakpoint()
+            
             features = np.vstack(features)
             qual = np.vstack(qual)
             query_seq = np.vstack(query_seq)
             ref_seq = np.vstack(ref_seq)
             id = np.hstack(id)
+
+            breakpoint()
 
     # checks if results folder exists, creates otherwise
     # check if plot dir exists, creates it otherwise
