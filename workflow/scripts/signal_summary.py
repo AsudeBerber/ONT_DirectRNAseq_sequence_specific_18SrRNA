93ccc8bfad233ed19613e30bfbae9f32c511de5f
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-11 10:39:03 +0200

    debug

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -143,6 +143,7 @@ def main(argv=sys.argv[1:]):
                 per_site_query_seq = np.array([list(read.query_sequence[locus-extra_window: locus+motif_length+extra_window]) for locus in loci])
                 if per_site_query_seq.dtype == "object":
                     breakpoint()
+                    pass
                 seq_dict = dict((x, z) for x, y, z in aligned_pairs)
                 per_site_ref_seq = np.array([[seq_dict[key] for key in range(locus-extra_window, locus+motif_length+extra_window)] for locus in loci])
             except:
