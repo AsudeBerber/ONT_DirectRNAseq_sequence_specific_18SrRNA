b4161c19f771b308456fdc132db8625aca292c7e
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-11 09:18:51 +0200

    debug

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -160,7 +160,6 @@ def main(argv=sys.argv[1:]):
                     try:
                         pod5_record = next(pod5.reads(selection=[read.query_name])) 
                         events = get_events(pod5_record.signal, read.get_tag("mv"), read.get_tag("ts"))
-                        breakpoint()
                         per_site_features = np.array([events[locus-extra_window: locus+motif_length+extra_window] for locus in loci])
                         per_site_id = np.array([read.query_name + ':' + str(locus) for locus in loci])
 
