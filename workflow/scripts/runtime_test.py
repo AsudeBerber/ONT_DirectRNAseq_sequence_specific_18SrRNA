b9523e2d137f901df1fb93033c4a43140d95531e
JensNMartin <jmarti167@gmail.com>
2024-06-14 09:32:14 +0200

    test

--- workflow/scripts/runtime_test.py
+++ workflow/scripts/runtime_test.py
@@ -1,9 +1,12 @@
 import json
 import pod5 as p5
 import time
+import pdb
+import timeit
+from sortedcontainers import SortedDict
 import os
 
-read_ID = "b0d44b01-953e-4020-9c91-350f0e16e138"
+read_ID = "fffff88b-c3c0-4ed3-ae1a-afff6053366a"
 
 pod5_path = "resources/pod5/p2s/"
 
@@ -11,17 +14,35 @@ time_st = time.process_time()
 with open('resources/results/p2s/pod5.json', "r") as f:
     pod5_index= json.load(f)
 
+pod5_index_sd = SortedDict(pod5_index)
+
+
+
+
+
+time_st = time.time_ns()
+
 pod5_file = pod5_index[read_ID]
+
+time_index_unsrt = time.time_ns() - time_st
+
+
+time_st = time.time_ns()
+
+pod5_file = pod5_index_sd[read_ID]
+
+time_index_srt = time.time_ns() - time_st
+
 
 time_index_load = time.process_time() - time_st
 
+breakpoint()
 time_st = time.process_time()
 #loops through all pod5 files in folder 
 with p5.Reader(pod5_file) as pod5:
         # Read the selected read from the pod5 file
         # next() is required here as Reader.reads() returns a Generator
-        with p5.Reader(read_path) as reader:
-            for rname in reader.read_ids:
+    with p5.Reader(read_path) as reader:
             try:
                 pod5_record = next(pod5.reads(selection=[read_ID])) 
                 idk = (pod5_record.signal)
