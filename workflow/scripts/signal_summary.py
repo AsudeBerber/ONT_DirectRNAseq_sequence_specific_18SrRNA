6a2cb6c992d65ae7dc5e308ad4bbac3ee6f60349
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-17 16:43:44 +0200

    fix

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -71,8 +71,7 @@ def get_events(signal, moves, offset, rev_loci, motif_length, extra_window):
     dict_events = {}
 
     # code would be faster if this was a dictionary with only relevant positions ({locus: event})
-    breakpoint()
-    pos_get_signal = list([range(locus - extra_window, locus + motif_length+extra_window) for locus in rev_loci])
+    pos_get_signal = [list(range(locus - extra_window, locus + motif_length+extra_window)) for locus in rev_loci]
     
     for locus in pos_get_signal:
 
@@ -86,7 +85,6 @@ def get_events(signal, moves, offset, rev_loci, motif_length, extra_window):
         data_tmp[5]=np.mean(signal[prev:sig_end])
         data_tmp[6]=np.std(signal[prev:sig_end])
         data_tmp[7]=np.median(signal[prev:sig_end])
-        breakpoint()
         data_tmp[8]=np.median(np.abs(signal[prev:sig_end]-data_tmp[4]))
         
         # get the mean signal for each quarter of the base signal
