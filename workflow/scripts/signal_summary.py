b505c6c0fa0a6259bdf0fda48e45f9dfce4b3be8
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-17 16:31:04 +0200

    test

--- workflow/scripts/signal_summary.py
+++ workflow/scripts/signal_summary.py
@@ -72,7 +72,7 @@ def get_events(signal, moves, offset, rev_loci):
 
     # code would be faster if this was a dictionary with only relevant positions ({locus: event})
     breakpoint()
-    pos_get_signal = list([locus +extra_window: locus +motif_length-extra_window:-1] for locus in rev_loci)
+    pos_get_signal = [[locus +extra_window: locus +motif_length-extra_window:-1] for locus in rev_loci]
     
     for locus in pos_get_signal:
 
