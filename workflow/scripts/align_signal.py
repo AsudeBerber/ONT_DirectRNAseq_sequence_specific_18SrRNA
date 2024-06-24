2d70a2b93892db89e75962085231513d365a3e22
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-24 17:13:33 +0200

    sync

--- workflow/scripts/align_signal.py
+++ workflow/scripts/align_signal.py
@@ -60,3 +60,44 @@ def get_events(signal, moves, offset, rev_loci, motif_length=1, extra_window=21,
         pass
 
     else: raise Exception("signal_stats has to be either True or False")
+
+
+def seq_to_mv(reads_ids, region, sample, seq=None, mv=None, ts=0, fetch = True, pos=42):
+if fetch == True:
+    seq, mv, ts, pos_read, ref_seq = bam_aligned(sample, reads_ids, region, pos)
+
+seq = seq[::-1] #sequence order is 5' -> 3', mv and signal are 3' -> 5': therefore sequence is turned around
+s = mv[0] #stride length
+p = 1 #itinerates through movetable array
+x = 0 #number of additional strides (stride amount - 1)
+start = ts + 1
+
+seq2mv = np.array([[1, ts, "-", "-"]])
+for i, base in enumerate(seq):
+    # print (base)
+    while p < len(mv)-1 and mv[p + 1] == 0: #last movetable index(p): mv[p+1] doesn't exist, would cause error
+        x = x + 1 #counts 0's
+        p = p + 1 #0 found -> movetable index moves by one
+        # print (f"{p},{x}\n")
+    p = p + 1 
+    # print(f"p={p}")
+    # print(f"x={x}")
+    end = start + (s-1) + (x*s) #stride 1: start + 4 (as start number is already first position in stride), all further strides: additional +5
+    x = 0 #resets number of additional strides
+    ref_base = ref_seq [i]
+    seq2mv = np.append(values=[[start, end, base, ref_base]], arr=seq2mv, axis=0) # appends 
+    start = end+1 #next base starts 1 after end of previous base
+
+print ("sequence-to-signal alignment finished,", len(seq), "bases, signal length =", end)    
+return seq2mv, pos_read
+
+
+def reverse_seq_mv(seq2mv):
+k = int(seq2mv[-1][1]) #signal range
+rev_seq2mv = np.flip(seq2mv,0)
+for base_data in rev_seq2mv:
+    start_old = int(base_data[0])
+    end_old = int(base_data[1])
+    base_data[0] = k - end_old #start and end switch when string is read in other direction, former last base is now first and vice versa
+    base_data[1] = k - start_old
+return rev_seq2mv
