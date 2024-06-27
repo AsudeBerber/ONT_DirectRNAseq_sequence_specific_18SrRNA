16c11e20fb37f551e245056ea0e999d4c9f7ed77
JensNMartin <166015578+JensNMartin@users.noreply.github.com>
2024-06-27 16:07:12 +0200

    fix

--- workflow/scripts/test.py
+++ workflow/scripts/test.py
@@ -104,11 +104,7 @@ def bam_aligned(sample, read_ids, region, pos):
             continue
 
 
-def main(argv = sys.argv[1:]):
-    args, fetch = cmd_parser(argv= argv)
 
-    seq2mv, pos_read = seq_to_mv(reads_ids = args.readID, region = args.region, sample = args.sample,
-                    seq = args.seq, mv = args.mv, ts = args.ts, fetch = fetch, ref_pos = args.pos, range = args.range) 
 
 
 def seq_to_mv(reads_ids, region, sample, seq=None, mv=None, ts=0, fetch = True, ref_pos=1841, range = 8):
@@ -127,6 +123,128 @@ def seq_to_mv(reads_ids, region, sample, seq=None, mv=None, ts=0, fetch = True, 
     pass
 
 
+#plots array of [start, end, base] to position (start - end) in signal
+def plot_signal_plus_seq(seq2mv, read_ids, pos, pos_read, range_bp, sequencer, full_read=False, range_var = "bases", pod5_dir = args.pod5_dir):
+     
+    if pod5_dir == None:
+        pod5_dir = "resources/pod5"
+    else: 
+        pod5_dir = f"{pod5_dir}"
+
+    #for output file naming
+
+    start = pos_read - range_bp
+    end = pos_read + range_bp
+
+    start_b = start
+    end_b = end
+
+    for filename in os.listdir(pod5_dir): #loops through all pod5 files in folder 
+        pod5_file = os.path.join(pod5_dir, filename)
+        with p5.Reader(pod5_file) as reader:
+            # Read the selected read from the pod5 file
+            # next() is required here as Reader.reads() returns a Generator
+            try:
+                read = next(reader.reads([read_ids]))
+            except:
+                continue
+            #
+            signal = read.signal
+            signal = signal[::-1] #reverses signal as RNA is sequenced 3' -> 5' but sequence is read 5' -> 3'
+#
+            # when range is set by bases, starting and ending time are pulled from seq2mv array --> base range is converted to time range
+            time = np.arange(len(signal)) #arbitrary time units
+            if range_var == "bases":
+                print("plot for base range", start, "-", end)
+                start = int(seq2mv[start][0])
+                end = int(seq2mv[end][1])
+                print("corresponding time range:", start,"-", end)
+            elif range_var == "time":
+                pass
+            else:
+                raise Exception ("range_var has to be bases or time")
+            #
+            if full_read == True:
+                start = 1
+                end = len(signal)
+            #
+            signal_slice = signal[start:end]
+            time = np.arange(len(signal)) #arbitrary time units
+            time_slice = time[start:end]
+
+            
+            #
+
+        #constructs colormap for plot
+        cmap_plot = list(range(range_bp*2 + 1))
+        for i in  cmap_plot:
+            if i == range_bp:
+                cmap_plot[i] = "#b2182b"
+            elif i %2 == 0:
+                cmap_plot[i] = "#2166ac"
+            else:
+                cmap_plot[i] = "#67a9cf"
+                
+
+        # Plot using matplotlib
+
+        # for powerpoint slide:
+        # fig, ax = plt.subplots(figsize=(18, 12))    
+        # viridis = colormaps["viridis"].resampled(range_bp*2 +1)
+
+        fig, ax = plt.subplots(figsize=(18, 4))
+        #
+        ax.plot (time_slice, signal_slice,linewidth = 1, color = "#B9B9B9", zorder = 1)
+        for i, base in enumerate(range(start_b, end_b +1)):
+                    # like slice above, just for every base -> signal per base can be colored differently
+                    base_start = int(seq2mv[base][0])
+                    base_end = int(seq2mv[base][1])
+                    signal_slice_base = signal[base_start:base_end]
+                    time_slice_base = time [base_start:base_end]
+                    ax.scatter(time_slice_base, signal_slice_base,
+                                linewidth = 1, marker= "o", facecolor = cmap_plot[i], zorder = 2, alpha = 0.5, edgecolor = "none")
+                                # linewidth = 1, marker= "o", facecolor = viridis.colors[i], zorder = 2, alpha = 0.5, edgecolor = "none", s = 600)
+            
+        
+        
+        
+        ax.margins(0.05, 0.1)
+        ax.set(xlabel = "base", ylabel = "signal (pA)")
+        plt.title(str("Read ID: "+ read_ids))
+        ax.annotate(f"18S RNA transcript - position {pos+1} ± {range_bp} bp", xy= (0.5, 0.95), xycoords="axes fraction", ha = "center")
+#
+        #annotation of bases to signal plot
+        i = -range_bp
+        xticks = []
+        for base_data in seq2mv:
+            x_coord = (int(base_data[0])+int(base_data[1]))/2 
+            if x_coord < start:
+                pass
+            elif x_coord > start and x_coord < end: 
+                # read seq
+                ax.annotate(base_data[2], xy = (x_coord, 0.02), fontsize = 8, xycoords=("data", "axes fraction"), ha = "center", color = base_color(base_data[2]))
+                # ref seq
+                # ax.annotate(base_data[3], xy = (x_coord, 0.06), fontsize = 8, xycoords=("data", "axes fraction"), ha = "center", color = "grey")
+                ax.annotate(i, xy= (x_coord, -0.04), fontsize = 8, xycoords=("data", "axes fraction"), ha = "center")
+                ax.axvline(int(base_data[0])-0.5, linestyle = ":", linewidth = 0.5, color = "lightgrey")
+                xticks.append(int(base_data[0])-0.5)
+                i = i + 1
+            else:
+                ax.axvline(int(base_data[0])-0.5, linestyle = ":", linewidth = 0.5, color = "lightgrey")
+                xticks.append(int(base_data[0])-0.5)
+                break
+        
+        ax.set_xticks(ticks = xticks)
+        ax.set_xticklabels([])
+
+        # check if plot dir exists, creates it otherwise
+        if not os.path.isdir(f"resources/signal/{sequencer}/plots/{read_ids}"):
+            os.makedirs(f"resources/mapped/signal/{sequencer}/plots/{read_ids}")
+
+        plt.savefig(f"resources/signal/{sequencer}/plots/{read_ids}/{read_ids}_{pos+1}-pm{range_bp}.svg", dpi = 300)           
+        plt.show()
+
+
 def cmd_parser(argv):
     parser = argparse.ArgumentParser(description="plots electric current/timepoint with corresponding basecalled base for given Read_ID")
     parser.add_argument("--sequencer", help= "name of sequencer (p2i|p2s)")
@@ -140,7 +258,7 @@ def cmd_parser(argv):
     parser.add_argument("--ts", help= "", action="store_true")
     parser.add_argument("--get_readids", help= "store name of all read ids in .txt", action= "store_true")
     parser.add_argument("--region", type= str, action= "store")
-    parser.add_argument("--pod5_dir", type= str, action= "store")
+    parser.add_argument("--pod5-dir", type= str, action= "store")
     args = parser.parse_args()
 
     if args.sequencer == None:
@@ -158,8 +276,20 @@ def cmd_parser(argv):
     else:
         fetch = True 
 
+    if args.pod5_dir == False:
+        args.pod5_dir = "pod5" 
+
     return args, fetch
 
+
+def main(argv = sys.argv[1:]):
+    args, fetch = cmd_parser(argv= argv)
+
+    seq2mv, pos_read = seq_to_mv(reads_ids = args.readID, region = args.region, sample = args.sample,
+                    seq = args.seq, mv = args.mv, ts = args.ts, fetch = fetch, ref_pos = args.pos, range = args.range) 
+    
+    plot_signal_plus_seq(seq2mv=seq2mv, read_ids = args.read_ID, pos= pos_read, range_bp = range, sequencer = args.sequencer, full_read=False, range_var = "bases", pod5_dir = args.pod5)
+    
 
 if __name__ == "__main__":
     exit(main())
